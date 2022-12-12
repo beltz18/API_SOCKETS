7685b1efd4dc392d773398331d2d1a3447e3c464
Beltz Anhxton <beltz.18kyodai@gmail.com>
2022-12-12 10:16:21 -0400

    sockets added to send informacion about users, transactions and applicants to client

--- src/server.js
+++ src/server.js
@@ -3,19 +3,65 @@ const app        = express()
 const http       = require("http")
 const { Server } = require("socket.io")
 const cors       = require("cors")
+const axios      = require("axios")
 app.use(cors())
 const server     = http.createServer(app)
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "*",
     methods: ["POST", "GET"]
   }
 })
 
-io.on("connection", socket => {
-  console.log(socket.id)
+io.on("connection", (socket) => {
+  console.log(`User connected from: ${socket.id}`)
 
+  socket.on("new_applicant", (data) => {
+    socket.broadcast.emit("applicant", data)
+  })
+
+  socket.on("me", (data) => {
+    axios.get('http://localhost:5000/select/transactions')
+      .then((res) => {
+        if (res.data) {
+          const dataT = res.data
+          socket.emit("data_transactions", dataT)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  
+    axios.get('http://localhost:5000/select/all')
+      .then((res) => {
+        if (res.data) {
+          const arr  = res.data
+          const me   = data.me
+          const rows = []
+          for (let i = 0; i < arr.length; i++) {
+            let rand1      = Math.round(Math.random()*100)
+            let rand2      = Math.round(Math.random()*10)
+            let obj        = {
+              id: arr[i]._id,
+              name: arr[i].name,
+              email: arr[i].email,
+              age: rand1,
+              hours: rand2,
+              status: "online"
+            }
+            if (arr[i].email !== me) {
+              rows.push(obj)
+            }
+          }
+          console.log("here", rows)
+          socket.emit("data_workers", rows)
+        }
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  })
 })
 
 server.listen(5300, () => {

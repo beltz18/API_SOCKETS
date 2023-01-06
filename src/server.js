c85ac0f86ad59f93eb4fefea725081c66089dcdd
Beltz Anhxton <beltz.18kyodai@gmail.com>
2023-01-06 03:19:57 -0400

    setted sockets for check-in and check-out functions to drive data to client

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const http       = require("http")
 const { Server } = require("socket.io")
 const cors       = require("cors")
 const axios      = require("axios")
+const moment     = require("moment")
 app.use(cors())
 const server     = http.createServer(app)
 
@@ -16,7 +17,7 @@ const io = new Server(server, {
   }
 })
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log(`User connected from: ${socket.id}`)
 
   socket.on("new_applicant", (data) => {
@@ -64,8 +65,22 @@ io.on("connection", (socket) => {
         console.log(err)
       })
   })
+
+  socket.on("checkin", async (data) => {
+    const checkin = await axios.post(process.env.API+process.env.CHECKIN, data)
+    socket.emit("checkin_msg", checkin.data.message)
+    const data_checkin = await axios.post(process.env.API+process.env.CHECKIN_SELECT, { email: data.email, dayCheckin: data.dayCheckin })
+    socket.emit("data_checkin", data_checkin.data)
+  })
+
+  socket.on("checkout", async (data) => {
+    const checkout = await axios.post(process.env.API+process.env.CHECKOUT, data)
+    socket.emit("checkout_msg", checkout.data.message)
+    const data_checkout = await axios.post(process.env.API+process.env.CHECKOUT_SELECT, {email: data.email, dayCheckOut: data.dayCheckOut})
+    socket.emit("data_checkout", data_checkout.data)
+  })
 })
 
 server.listen(5300, () => {
-  console.log("Server running on http://localhost:5300")
+  console.log(`Server running on ${process.env.URL+process.env.PORT}`)
 })
